4115b24dee03b12fe00b568174aa0ec00ecb63a4
RaedN0 <marcel.schoerghuber@gmail.com>
2025-05-08 13:24:33 +0200

    Set Neo-Tree width to 50

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -107,7 +107,7 @@ return {
       -- Add this section and customize to your liking
       window = {
         position = 'left',
-        width = 30,
+        width = 50,
         mapping_options = {
           noremap = true,
           nowait = true,
